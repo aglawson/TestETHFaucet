9d224473adf4078535901922e0cf5761343f3ffb
Adrian Lawson <adrianglawson@gmail.com>
2023-09-20 09:13:03 -0700

    frontend logic and styling

---
+++ frontend/client/api/functions.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const apiUrl = 'https://aglawson.uw.r.appspot.com';
+// const apiUrl = 'http://localhost:8080'
+export const getAuthUrl = async() => {
+    const response = await axios.get(`${apiUrl}/get_auth_url`);
+    return response.data;
+}
+
+export const TwitterLookup = async (token, token_secret) => {
+    const result = await axios.get(`${apiUrl}/twitter_lookup?oauth_token=${token}&oauth_token_secret=${token_secret}`)
+    return result.data;
+}
+
+export const UpdateUser = async(wallet, attribute, value) => {
+    const result = await axios.get(`${apiUrl}/update_user?wallet=${wallet}&attribute=${attribute}&value=${value}`)
+    return result.data;
+}
+
+export const getAccessToken = async (pin, requestToken) => {
+    let enc_token = requestToken;
+    const result = await axios.get(`${apiUrl}/get_access_token?pin=${pin}&requestToken=${enc_token}`)
+    return result.data;
+}
+
+export const Drip = async (twitter, wallet) => {
+    const result = await axios.get(`${apiUrl}/drip?twitter=${twitter}&wallet=${wallet}`);
+    return result.data;
+}
+
+export const AddFaucetUser = async (wallet, twitter) => {
+    const result = await axios.get(`${apiUrl}/add_faucet_user?handle=${twitter}&wallet=${wallet}`);
+    return result.data
+}
+
+export const GetUser = async (wallet) => {
+    const result = await axios.get(`${apiUrl}/get_user?wallet=${wallet}`);
+    return result.data
+}
--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,33 +1,191 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {ethers} from 'ethers'
+import { ToastContainer, toast } from 'react-toastify';
+import { getAuthUrl, TwitterLookup, UpdateUser, getAccessToken, Drip, AddFaucetUser, GetUser } from '../api/functions';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
+let provider, signer = null;
+const explorerURL = 'https://sepolia-blockscout.scroll.io/tx/'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [userAddress, setUserAddress] = useState(null);
+  const [authUrl, setAuthUrl] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [twitter, setTwitter] = useState(null);
+  const [message, setMessage] = useState('Connect Wallet');
+  const [copy, setCopy] = useState('Welcome to this humble establishment. It appears you need some more test ETH to continue building your next groundbreaking app. Feel free to top up every 24 hours! Simply connect your wallet to continue.')
+
+  async function init() {
+    if(userAddress !== null) return;
+
+    if (window.ethereum == null) {
+      toast.error("MetaMask not installed");
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      const nw = await provider.getNetwork();
+      signer = await provider.getSigner();
+
+      // Check if the network is correct, if not, change it
+      if (Number(nw.chainId) !== 534351) {
+        toast.warn('Switching Networks')
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x8274f' }],
+          })
+          // window.location.reload();
+          provider = new ethers.BrowserProvider(window.ethereum);
+
+        } catch (switchError) {
+          console.log({ switchError })
+          if (switchError.code === 4902) {
+            try {
+              await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [{ 
+                  chainId: '0x8274f',
+                  chainName: 'Scroll Sepolia Testnet',
+                  nativeCurrency: {
+                    name: 'ETH',
+                    symbol: 'ETH',
+                    decimals: 18
+                  },
+                  rpcUrls: ['https://sepolia-rpc.scroll.io'],
+                  blockExplorerUrls: ['https://sepolia-blockscout.scroll.io']
+                }],
+              });
+              window.location.reload();
+  
+              provider = new ethers.BrowserProvider(window.ethereum);
+            } catch (addError) {
+              console.error(addError);
+            }
+          }
+        }
+      }
+
+      signer = await provider.getSigner();
+      setUserAddress(await signer.getAddress());
+      const user = await GetUser(await signer.getAddress());
+      console.log(user)
+      if(user !== null) {
+        setTwitter(user.handle)
+        toast.success(`Welcome Back ${user.handle}!`);
+        setCopy(`Good to see ya again, ${user.handle}. Click the Drip button and you'll have your test ETH in no time.`);
+        return;
+      }
+      setMessage('Connect X');
+      toast.success("Wallet Connected!");
+      setCopy('Great! Now just connect your X account. This is to prevent thievery. We gotta be careful out here nowadays.');
+    }
+  }
+
+  async function twitterStep1() {
+    if(userAddress == null) {
+      await init()
+    }
+    if(twitter != null) {
+      return
+    }
+    const data = await getAuthUrl()
+    console.log(data)
+    let token = data.requestToken.oauth_token
+    let token_secret = data.requestToken.oauth_token_secret
+    setAuthUrl(data.authUrl)
+    let accessTokenObj = {
+        token,
+        token_secret
+    }
+    setAccessToken(accessTokenObj)
+  }
+
+  async function twitterStep2(event, requestToken) {
+    event.preventDefault()
+    try{
+    const pin = document.getElementById('pin').value
+    const accessToken = await getAccessToken(parseInt(pin), requestToken.token)
+
+    let oauth_token = accessToken.oauth_token
+    let oauth_token_secret = accessToken.oauth_token_secret
+
+    const result = await TwitterLookup(oauth_token, oauth_token_secret)
+    const update = await UpdateUser(userAddress, 'twitter', result.data.username)
+    const updateUserId = await UpdateUser(userAddress, 'twitter_id', result.data.id)
+    const setAuthToken = await UpdateUser(userAddress, 'oauth_token', oauth_token)
+    const setAuthTokenSecret = await UpdateUser(userAddress, 'oauth_token_secret', oauth_token_secret)
+
+    setTwitter(result.data.username)
+    setAuthUrl(null)
+    const addUser = await AddFaucetUser(userAddress, result.data.username)
+    console.log(addUser)
+    toast.success('X Connected!')
+    setCopy(`Pleasure to meet you, ${user.handle}. Click the Drip button and you'll have your test ETH in no time.`);
+    return result
+    } catch (error) {
+        console.log(error)
+        toast.error(error)
+    }
+  }
+
+  async function drip() {
+    const tx = await Drip(twitter, userAddress);
+    console.log(tx)
+
+    if(tx.error) {
+      if(tx.error.error) {
+        toast.error(tx.error.error.reason);
+        if(tx.error.error.reason.includes('come back later')) {
+          setCopy(`Whoa there, looks like you've had your fill for today. Come back again tomorrow.`)
+        }
+        return;
+      }
+      toast.error(tx.error);
+      return;
+    }
+
+    setCopy(`Your ETH has been delivered. Happy building, hope to see you tomorrow!`)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      
+      <p style={{padding: '5px', backgroundColor: '#C68642',position: 'fixed', top: '-1%', left: '2%', fontFamily: 'Courier New, monospace'}}>Scroll Sepolia Faucet</p>
+      <div style={{padding: '5px', backgroundColor: '#C68642',position: 'fixed', top: '7%', left: '2%', fontFamily: 'Courier New, monospace'}}><a href="https://twitter.com/0xlawson" target="_blank" rel="noopener noreferrer" style={{color: 'white', textDecoration: 'none'}}>Follow creator on X</a></div>
+
+      <div style={{position: 'fixed', top: '1%', right: '2%'}}>
+        <button onClick={() => userAddress == null ? init() : twitter == null ? twitterStep1() : twitter} style={{fontFamily: 'Courier New, monospace'}}> 
+          {message}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <div style={{position:'fixed', backgroundColor: 'snow', width: '300px', height: '400px', border: '1px solid black', padding: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+        <h2 style={{color: 'black'}}>0.01 ETH / 24hrs</h2>
+        <strong style={{display: authUrl == null ? '' : 'none' ,fontFamily: 'Courier New, monospace', color: 'black', marginTop: '-25%'}}>{copy}</strong>
+        <p style={{color: 'black', display: authUrl == null ? 'none' : ''}}>Click <a href={authUrl} target='_blank'>here</a> to sign into X, then copy the code provided by X and paste it below.</p>
+        <input id='pin'style={{display: authUrl == null ? 'none' : '', marginTop: '15%', width: '60%', height: '10%', fontFamily: 'Courier New, monospace', color: 'black', backgroundColor: 'white', margin: 'auto'}} type="text" placeholder="Auth Code"/>
+        <button onClick={() => userAddress == null ? init() : twitter == null ? twitterStep1() : twitter} style={{fontFamily: 'Courier New, monospace', display: twitter == null && authUrl == null ? '' : 'none'}}>
+          {message}
+        </button>
+        <button style={{fontFamily: 'Courier New, monospace', display: authUrl == null ? 'none' : '' }} onClick={(e) => twitterStep2(e, accessToken)}>{'Submit Code'}</button>
+        <button style={{fontFamily: 'Courier New, monospace', display: userAddress !== null && twitter !== null ? '' : 'none'}} onClick={() => drip()}>Drip</button>
+        </div>
+      </div>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />    
     </>
   )
 }
